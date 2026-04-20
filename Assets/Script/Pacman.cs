22b527050866297c459e0aaf715f60ee8551f44c
levuchaucuong160106-eng <levuchaucuong160106@gmail.com>
2026-04-20 09:56:55 -0400

    AddNewSound
    -Stepping Sound
    -Pellet eating sound
    -Background music sound

--- Assets/Script/Pacman.cs
+++ Assets/Script/Pacman.cs
@@ -7,7 +7,12 @@ public class Pacman : MonoBehaviour
     public Sprite up;
     public Sprite down;
     public AnimateSprite anim;
+<<<<<<< Updated upstream
 
+=======
+    public bool Teleportable = true;
+    public AudioSource audio;
+>>>>>>> Stashed changes
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
@@ -25,6 +30,7 @@ public class Pacman : MonoBehaviour
             anim.enabled = false;
             this.movement.SetDirection(Vector2.up);
             spriteRenderer.sprite = up;
+            audio.Play();
             
         }
         else if (Input.GetKeyDown(KeyCode.S))
@@ -33,7 +39,7 @@ public class Pacman : MonoBehaviour
             anim.enabled = false;
             this.movement.SetDirection(Vector2.down);
             spriteRenderer.sprite = down;
-            
+            audio.Play();
         }
         else if (Input.GetKeyDown(KeyCode.D))
         {   
@@ -41,6 +47,7 @@ public class Pacman : MonoBehaviour
             anim.Restart();
             this.movement.SetDirection(Vector2.right);
             this.spriteRenderer.flipX = true;
+            audio.Play();
         }
         else if (Input.GetKeyDown (KeyCode.A))
         {
@@ -48,6 +55,7 @@ public class Pacman : MonoBehaviour
             anim.Restart();
             this.movement.SetDirection(Vector2.left);
             this.spriteRenderer.flipX = false;
+            audio.Play();
         }
         //float angle = Mathf.Atan2(this.movement.Dir.y, this.movement.Dir.x);
         //this.transform.rotation = Quaternion.AngleAxis(angle * Mathf.Rad2Deg, Vector3.forward);
